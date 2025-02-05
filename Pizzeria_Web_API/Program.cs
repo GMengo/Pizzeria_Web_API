a9aa80fb29cc93f6cea0b489de66ccbed7efb24e
giovanni.mengoni <giovannimengoni@gmail.com>
2025-02-05 16:56:29 +0100

    Add project files.

---
+++ Pizzeria_Web_API/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pizzeria_Web_API
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
